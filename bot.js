208b967c05e62b5098acf2d03bdfcd2d915346e5
nubmaster4568 <144072936+nubmaster4568@users.noreply.github.com>
2024-07-23 13:46:20 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,33 @@
 
+const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
+const app = express();
+const port = process.env.PORT || 3000;
+
 const token = '6911216755:AAGlDbzA5WLa0bZlMk2iv3dx7T2vko55sXc';
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id; // Corrected variable name
-  bot.sendMessage(chatId, 'SHOP', {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: 'SHOP',
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'SHOP', {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: 'SHOP',
             web_app: { url: `https://lk-rt5d.onrender.com/admin/index.html?userId=${chatId}` }
-          }
-        ]
-      ]
-    }
-  });
+                    }
+                ]
+            ]
+        }
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send('Telegram Bot is running');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
