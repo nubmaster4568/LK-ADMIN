78e836ffe92c2da1a5f61958eb1b3a754b83b8cd
nubmaster4568 <144072936+nubmaster4568@users.noreply.github.com>
2024-07-23 13:27:01 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,20 +3,19 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '6911216755:AAGlDbzA5WLa0bZlMk2iv3dx7T2vko55sXc';
 const bot = new TelegramBot(token, { polling: true });
 
-bot.start((ctx) => {
-    const chatId = ctx.chat.id; // Get chat ID from the context
-    ctx.reply('SHOP', {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: 'SHOP',
-                        web_app: { url: `https://lk-rt5d.onrender.com/admin/index.html?userId=${chatId}` }
-                    }
-                ]
-            ]
-        }
-    });
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id; // Corrected variable name
+  bot.sendMessage(chatId, 'SHOP', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'SHOP',
+            web_app: { url: `https://lk-rt5d.onrender.com/admin/index.html?userId=${chatId}` }
+          }
+        ]
+      ]
+    }
+  });
 });
 
-bot.launch();
