f60dba26ebcb6f7a651b6cc04df2f88f3c564a6f
nubmaster4568 <144072936+nubmaster4568@users.noreply.github.com>
2024-07-23 20:56:55 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,21 +7,28 @@ const port = process.env.PORT || 3000;
 const token = '7237894833:AAHB2FJLiU9-pczE_NDm5Xdcd6dBwHd6uBE';
 const bot = new TelegramBot(token, { polling: true });
 
+
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    console.log(chatId)
-    bot.sendMessage(chatId, 'SHOP', {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: 'SHOP',
-            web_app: { url: `https://lk-rt5d.onrender.com/admin/index.html?userId=${chatId}` }
-                    }
+
+    // Check if the chat ID matches the specified ID
+    if (chatId === 6981136606) {
+        console.log(chatId);  // Log the chat ID
+        bot.sendMessage(chatId, 'SHOP', {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {
+                            text: 'SHOP',
+                            web_app: { url: `https://lk-rt5d.onrender.com/admin/index.html?userId=${chatId}` }
+                        }
+                    ]
                 ]
-            ]
-        }
-    });
+            }
+        });
+    } else {
+        console.log(`Ignored chat ID: ${chatId}`);
+    }
 });
 
 app.get('/', (req, res) => {
